7907cd602a4a64aad08b5ec958bc155bb5b6774e
arunarvindh05 <arunarvindh05@gmail.com>
2021-08-04 21:20:52 +0530

    Adding changes

---
+++ src/components/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder, deleteReminder, clearReminders } from '../actions'
+import {Glyphicon}  from 'react-bootstrap';
+import '../style.css';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+            dueDate: '',
+        }
+    }
+    
+    addReminder() {
+        if (new Date() <= new Date(this.state.dueDate)) {
+          var secondBetweenTwoDate = Math.abs(new Date().getTime() - (new Date(this.state.dueDate).getTime()));
+          console.log("data", secondBetweenTwoDate);
+
+          setTimeout(() => {
+            this.props.addReminder(this.state.text, this.state.dueDate);
+            alert(this.state.text, " Time as come now");
+          }, secondBetweenTwoDate);
+        }
+    else{
+        alert("date or time has already passed");
+    }
+    }
+
+    deleteReminder(id) {
+        this.props.deleteReminder(id);
+    }
+
+    renderReminders() {
+        const { reminders } = this.props;
+        return (
+            <ul className="list-group col-sm-4">
+                {
+                    reminders.map(reminder => {
+                        // debugger;
+                        return (
+                            <li key={reminder.id} className="list-group-item">
+                                <div className="list-item">
+                                    <div>{reminder.text }</div>
+                                    {/* <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div> */}
+                                </div>
+                                <div
+                                    className="list-item delete-button"
+                                    onClick={() => this.deleteReminder(reminder.id)}
+                                >
+                                <Glyphicon 
+                                    glyph="remove-sign" 
+                                    />
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <div className="App">
+                <div className="title">
+                    Reminder App
+                </div>
+                <div className="form-inline">
+                    <div className="form-group reminder-form">
+                        <input
+                            className="form-control"
+                            placeholder="I have to..."
+                            onChange={event => {this.setState({text: event.target.value})}}
+                            />
+                        <input
+                            className="form-control"
+                            type="datetime-local"
+                            onChange={event => {this.setState({dueDate: event.target.value})}}
+                            />
+                        <button
+                            type="button"
+                            className="btn btn-success"
+                            onClick={()=>this.addReminder()}>
+                            Add Reminder
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-danger"
+                            onClick={()=>this.props.clearReminders()}>
+                            Clear Reminders
+                        </button>
+                    </div>
+                </div>
+                {this.renderReminders()}
+            </div>
+        );
+    }
+}
+function mapStateToProps(state) {
+    return {
+        reminders: state
+    }
+}
+ 
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders})(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import reducertop from './reducers';
 
+// debugger;
+const store = createStore(reducertop,applyMiddleware(thunkMiddleware));
 ReactDOM.render(
-  <React.StrictMode>
+<Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+</Provider>
+, document.getElementById('root'));
